173154c12d36410e7993be95326f0b0939a0852e
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2023-10-19 17:41:15 +0200

    Toggle Button fixed
    Toggle button for Item selection is now fixed

--- TestApp_client_20231018125033/src/components/CVItemsPage.jsx
+++ TestApp_client_20231018125033/src/components/CVItemsPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   Container,
   Button,
+  
   Typography,
   List,
   LinearProgress,
@@ -14,36 +15,20 @@ import { DraftCVItem } from "./DraftCVItem";
 import { useShowLoader } from "../hooks/util-hooks";
 import { MoreInfo } from "./MoreInfo";
 import { getCVId } from "../utils";
-import { useState } from "react";
+
 
 export function CVItemsPage() {
   const { loading, CVs, ...CVActions } = useCVs();
   const { draftCVs, ...draftCVActions } = useDraftCVs();
   const showLoader = useShowLoader(loading, 200);
-
-  // //Pop up code for the CV form
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [selectedCVElement, setSelectedCVElement] = useState("name"); // Default value
-
-  const handleMenuOpen = () => {
-    setMenuOpen(true);
-  };
-
-  const handleMenuClose = () => {
-    setMenuOpen(false);
-  };
-
-  const handleMenuItemClick = (element) => {
-    setSelectedCVElement(element);
-    setMenuOpen(false);
-  };
-
-  const handleConfirmation = () => {
-    // Implement your logic for creating CV with the selected CV_element
-    console.log(`Creating CV with ${selectedCVElement}`);
-  };
-
-  //fin
+  
+  
+const [itemSelect, setItemSelect] = React.useState(false);
+const handleButtonClick = () => {
+  //   // Toggle the value of itemSelect when the button is clicked
+  setItemSelect(!itemSelect);
+};
+ 
 
   return (
     <Container className="main-container" maxWidth="sm">
@@ -61,10 +46,26 @@ export function CVItemsPage() {
             variant="contained"
             color="primary"
             startIcon={<AddIcon />}
-            onClick={() => draftCVActions.createDraftCV()}>
-            Add CV
+            onClick={() => handleButtonClick()}>
+           Choose CV Element
           </Button>
+
+          {itemSelect && <h1>Hello</h1>}
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+            onClick={() => draftCVActions.createDraftCV()}>
+            Add CV Item
+          </Button>
+
+
+         
           <List style={{ width: "100%" }}>
+           
+            
+            
+            
             {CVs.map((CV) => (
               <CVItem
                 key={getCVId(CV)}
@@ -101,47 +102,7 @@ export function CVItemsPage() {
             ))}
           </List>
           
-          {/* Pop up code for the CV form */}
-          <ListItem>
-            <ListItemIcon>
-              <Checkbox
-                data-testid="CV-checkbox"
-                edge="start"
-                color="primary"
-                checked={CV.isComplete}
-                onClick={() => {
-                  CVActions.toggleCV(CV);
-                }}
-              />
-            </ListItemIcon>
-            <ListItemText>{CV[selectedCVElement]}</ListItemText>
-            <ListItemSecondaryAction>
-              <IconButton
-                data-testid="CV-delete-button"
-                edge="end"
-                size="small"
-                onClick={() => {
-                  CVActions.deleteCV(CV);
-                }}>
-                <ClearIcon />
-              </IconButton>
-              <Button onClick={handleMenuOpen}>Change CV Element</Button>
-              <Menu
-                anchorEl={isMenuOpen}
-                keepMounted
-                open={Boolean(isMenuOpen)}
-                onClose={handleMenuClose}>
-                <MenuItem onClick={() => handleMenuItemClick("name")}>
-                  Name
-                </MenuItem>
-                <MenuItem onClick={() => handleMenuItemClick("surname")}>
-                  Surname
-                </MenuItem>
-                {/* Add more menu items as needed */}
-              </Menu>
-              <Button onClick={handleConfirmation}>Confirm</Button>
-            </ListItemSecondaryAction>
-          </ListItem>
+         
         </div>
       )}
       <MoreInfo />
