ec0c3d51592461b9b3f36da0f281c9869f2eab2c
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2023-10-20 09:59:14 +0200

    Fixed Unit Tests
    Fixed the existing unit tests for the projects

--- TestApp_client_20231018125033/src/components/ItemSelectMenu.jsx
+++ TestApp_client_20231018125033/src/components/ItemSelectMenu.jsx
@@ -10,6 +10,7 @@ import {
     MenuItem,
     SelectChangeEvent ,
     } from "@mui/material";
+import { CVItem } from "./CVItem";
 
 
 
@@ -17,12 +18,11 @@ export function ItemSelectMenu(){
     const [chosenElement, setChosenElement] = React.useState(null);
     const handleElementSelect = (element) => {
         setChosenElement(element);
-    };
+    };  
     return(
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={age}
           label="Item Select Menu"
         >
           <MenuItem onClick={() => handleElementSelect('name')}>Name</MenuItem>
