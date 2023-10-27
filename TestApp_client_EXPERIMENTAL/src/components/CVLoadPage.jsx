0b133b56193df42c8402fdf27b8ea923ab15ed7d
trebeyro <79139311+trebeyro@users.noreply.github.com>
2023-10-27 10:44:29 +0200

    Gestion de l'affichage pour le fichier CSV concernant les publications
    + quelques correctifs d'affichage pour l'export PDF

--- TestApp_client_EXPERIMENTAL/src/components/CVLoadPage.jsx
+++ TestApp_client_EXPERIMENTAL/src/components/CVLoadPage.jsx
@@ -1,22 +1,42 @@
 import React, { useState, useEffect, useRef } from "react";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
+import Papa from "papaparse";
+import './CVLoadPage.css';
 
 export function CVLoadPage() {
   const [cvData, setCvData] = useState(null);
+  const [publicationsData, setPublicationsData] = useState(null);
+  const [showPublications, setShowPublications] = useState(false);
   const componentRef = useRef();
+  const csvLinkRef = useRef();
 
   const handlePrint = () => {
     if (componentRef.current) {
       const pdfWidth = 210;
       const pdfHeight = 297;
+      componentRef.current.classList.add("pdf-small-text");
       html2canvas(componentRef.current).then((canvas) => {
         const imgData = canvas.toDataURL("image/png");
         const pdf = new jsPDF("p", "mm", "a4");
         pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-        pdf.save("cv.pdf");
+        componentRef.current.classList.remove("pdf-small-text");
+        pdf.save(`CV_${cvData["Personal Information"]["name"]}.pdf`);
+        document.querySelector(".hide-in-pdf").classList.remove("hidden");
       });
+      document.querySelector(".hide-in-pdf").classList.add("hidden");
     }
+  };
+
+  const readCSV = () => {
+    Papa.parse("/citations.csv", {
+      download: true,
+      header: true,
+      complete: (result) => {
+        setPublicationsData(result.data);
+      },
+      dynamicTyping: true,
+    });
   };
 
   useEffect(() => {
@@ -33,102 +53,137 @@ export function CVLoadPage() {
     fetchCVData();
   }, []);
 
+  useEffect(() => {
+    readCSV();
+  }, []);
+
+  const togglePublications = () => {
+    setShowPublications(!showPublications);
+  };
+
   return (
     <div>
-      <button onClick={handlePrint}>Export PDF</button>
+      <button onClick={handlePrint}>Télécharger</button>
       <div ref={componentRef} style={{ width: "210mm", height: "297mm" }}>
-      {cvData && (
-        <div>
-          <h1>CV</h1>
-          <h2>Informations Personnelles</h2>
-          <ul>
-            <li>Nom : {cvData["Personal Information"]["name"]}</li>
-            <li>Prénom : {cvData["Personal Information"]["surname"]}</li>
-            <li>Date de Naissance : {cvData["Personal Information"]["dateBirth"]}</li>
-            <li>Mail : {cvData["Personal Information"]["Mail"]}</li>
-            <li>Téléphone : {cvData["Personal Information"]["Telephone"]}</li>
-            <li>Adresse : {cvData["Personal Information"]["Adresse"]}</li>
-            <li>Âge : {cvData["Personal Information"]["Age"]}</li>
-            <li>Nationalité : {cvData["Personal Information"]["Nationality"]}</li>
-          </ul>
-
-          <h2>Qualifications</h2>
+        {cvData && (
+          <div>
+            <h1>CV {cvData["Personal Information"]["name"]}</h1>
+            <h2>Informations Personnelles</h2>
             <ul>
-              {cvData["Qualifications"] &&
-                cvData["Qualifications"].map((qualification, index) => (
-                  <li key={index}>
-                    Date: {qualification["Date"] || "N/A"}
-                    {/* Le reste de vos éléments d'affichage pour les qualifications */}
-                  </li>
-                ))}
+              <li>Nom : {cvData["Personal Information"]["name"]}</li>
+              <li>Prénom : {cvData["Personal Information"]["surname"]}</li>
+              <li>Date de Naissance : {cvData["Personal Information"]["dateBirth"]}</li>
+              <li>Mail : {cvData["Personal Information"]["Mail"]}</li>
+              <li>Téléphone : {cvData["Personal Information"]["Telephone"]}</li>
+              <li>Adresse : {cvData["Personal Information"]["Adresse"]}</li>
+              <li>Âge : {cvData["Personal Information"]["Age"]}</li>
+              <li>Nationalité : {cvData["Personal Information"]["Nationality"]}</li>
             </ul>
 
-          <h2>Education</h2>
-            <ul>
-              {cvData["Education"] &&
-                cvData["Education"].map((education, index) => (
-                  <li key={index}>
-                    Institution: {education["Institution"] || "N/A"}
-                    Field: {education["Field"] || "N/A"}
-                    Date: {education["Date"] || "N/A"}
-                    Thesis: {education["Thesis"] || "N/A"}
-                    <h3>Supervisors</h3>
-                    <ul>
-                      {education["Supervisor"] &&
-                        education["Supervisor"].map((supervisor, index) => (
-                          <li key={index}>
-                            Name: {supervisor["Name"] || "N/A"}
-                            Title: {supervisor["Title"] || "N/A"}
-                          </li>
-                        ))}
-                    </ul>
-                    Funding: {education["Funding"] || "N/A"}
-                  </li>
-                ))}
-            </ul>
+            <h2>Qualifications</h2>
+            {cvData["Qualifications"] && cvData["Qualifications"].map((qualification, index) => (
+              qualification["Date"] && (
+                <ul key={index}>
+                  <li>Date: {qualification["Date"]}</li>
+                  {/* Le reste de vos éléments d'affichage pour les qualifications */}
+                </ul>
+              )
+            ))}
 
-          <h2>Experience</h2>
-            <ul>
-              {cvData["Experience"] &&
-                cvData["Experience"].map((experience, index) => (
-                  <li key={index}>
-                    {/* Affichez les détails de l'expérience ici */}
-                  </li>
-                ))}
-            </ul>
+            <h2>Education</h2>
+            {cvData["Education"] && cvData["Education"].map((education, index) => (
+              education["Institution"] && (
+                <ul key={index}>
+                  <li>Institution: {education["Institution"]}</li>
+                  <li>Field: {education["Field"]}</li>
+                  <li>Date: {education["Date"]}</li>
+                  <li>Thesis: {education["Thesis"]}</li>
+                  <h3>Supervisors</h3>
+                  <ul>
+                    {education["Supervisor"] && education["Supervisor"].map((supervisor, sIndex) => (
+                      supervisor["Name"] && (
+                        <li key={sIndex}>
+                          Name: {supervisor["Name"]}
+                          Title: {supervisor["Title"]}
+                        </li>
+                      )
+                    ))}
+                  </ul>
+                  <li>Funding: {education["Funding"]}</li>
+                </ul>
+              )
+            ))}
 
-          <h2>Publications</h2>
-            <ul>
-              {cvData["Publications"] &&
-                cvData["Publications"].map((publication, index) => (
-                  <li key={index}>
-                    {/* Affichez les détails de la publication ici */}
-                  </li>
-                ))}
-            </ul>
+            <h2>Experience</h2>
+            {cvData["Experience"] && cvData["Experience"].map((experience, index) => (
+              experience["Details"] && (
+                <ul key={index}>
+                </ul>
+              )
+            ))}
 
-          <h2>Professional Affiliations</h2>
-            <ul>
-              {cvData["Professional Affiliations"] &&
-                cvData["Professional Affiliations"].map((affiliation, index) => (
-                  <li key={index}>
-                    {/* Affichez les détails de l'affiliation ici */}
-                  </li>
-                ))}
-            </ul>
+            <h2>Publications</h2>
+            {publicationsData && (
+              <table>
+                <thead>
+                  <tr>
+                    <th>Auteurs</th>
+                    <th>Titre</th>
+                    <th>Publication</th>
+                    <th>Volume</th>
+                    <th>Nombre</th>
+                    <th>Page</th>
+                    <th>Année</th>
+                    <th>Editeur</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {publicationsData.slice(0, showPublications ? publicationsData.length : 2).map((publication, index) => (
+                    <tr key={index}>
+                      <td>{publication["Authors"] || ""}</td>
+                      <td>{publication["Title"] || ""}</td>
+                      <td>{publication["Publication"] || ""}</td>
+                      <td>{publication["Volume"] || ""}</td>
+                      <td>{publication["Number"] || ""}</td>
+                      <td>{publication["Pages"] || ""}</td>
+                      <td>{publication["Year"] || ""}</td>
+                      <td>{publication["Publisher"] || ""}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+            {publicationsData && publicationsData.length > 2 && (
+              <button onClick={togglePublications}>
+                {showPublications ? "Masquer Publications" : "Afficher Toutes les Publications"}
+              </button>
+            )}
+            <a
+              ref={csvLinkRef}
+              className="hide-in-pdf"
+              style={{ display: "none" }}
+              href="/citations.csv"
+              download={`CV_${cvData["Personal Information"]["name"]}.csv`}
+            ></a>
+          </div>
+        )}
 
-          <h2>Honors/Awards/Grants</h2>
-            <ul>
-              {cvData["Honors/Awards/Grants"] &&
-                cvData["Honors/Awards/Grants"].map((award, index) => (
-                  <li key={index}>
-                    {/* Affichez les détails de la récompense ici */}
-                  </li>
-                ))}
+        <h2>Professional Affiliations</h2>
+        {cvData && cvData["Professional Affiliations"] && cvData["Professional Affiliations"].map((affiliation, index) => (
+          affiliation["Details"] && (
+            <ul key={index}>
             </ul>
-        </div>
-      )}
-    </div>
+          )
+        ))}
+
+        <h2>Honors/Awards/Grants</h2>
+        {cvData && cvData["Honors/Awards/Grants"] && cvData["Honors/Awards/Grants"].map((award, index) => (
+          award["Details"] && (
+            <ul key={index}>
+            </ul>
+          )
+        ))}
+      </div>
     </div>
   );
 }
