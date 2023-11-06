e28ef3b129ccae3a8d4198bae3b756bef8f39e98
trebeyro <79139311+trebeyro@users.noreply.github.com>
2023-11-06 10:29:12 +0100

    ajout modules

--- TestApp_client_20231018125033/src/components/App.jsx
+++ TestApp_client_20231018125033/src/components/App.jsx
@@ -2,6 +2,7 @@ import { AppBar, Toolbar, Button, Typography } from "@mui/material";
 import { WelcomePage } from "./WelcomePage";
 import {FormElement} from "./FormElement";
 import { CVItemsPage } from "./CVItemsPage";
+import { CVLoadPage } from "./CVLoadPage";
 import { AppProvider, useApp } from "./RealmApp";
 import { ThemeProvider } from "./Theme";
 import { AppName } from "./AppName";
@@ -44,7 +45,7 @@ function App() {
       {/* If a user is signed in, load the CV Item Page. Else load the sign up/log in page */}
       {/* We can add another conditional statement here to render the CV visualizer here*/}
       {/* or add a button on the CV Composition page  */}
-      {currentUser ? <CVItemsPage /> : <WelcomePage />}
+      {currentUser ? <CVLoadPage /> : <WelcomePage />}
     </div>
   );
 }
