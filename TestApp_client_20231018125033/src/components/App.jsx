875f212cf9998ea10eb190b2d660d56d60326053
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2023-11-10 04:12:16 +0100

    Added simple routing between the pages

--- TestApp_client_20231018125033/src/components/App.jsx
+++ TestApp_client_20231018125033/src/components/App.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { AppBar, Toolbar, Button, Typography } from "@mui/material";
 import { WelcomePage } from "./WelcomePage";
-import {FormElement} from "./FormElement";
 import { CVItemsPage } from "./CVItemsPage";
+import { CVLoadPage } from "./CVLoadPage";
+import { CVLoadPageTest } from "./CVLoadPageTest";
 import { AppProvider, useApp } from "./RealmApp";
 import { ThemeProvider } from "./Theme";
 import { AppName } from "./AppName";
@@ -21,6 +23,14 @@ export default function ProvidedApp() {
 
 function App() {
   const { currentUser, logOut } = useApp();
+   const [page, setPage] = React.useState("CVItemsPage");
+   const handlePageChange = () => {
+     if (page === "CVItemsPage") {
+       setPage("CVLoadPage");
+     } else {
+       setPage("CVItemsPage");
+     }
+   }
   return (
     <div className="App">
       
@@ -29,22 +39,33 @@ function App() {
         <Toolbar>
           <AppName />
           {currentUser ? (
-            <Button
+            
+            <><Button
+              variant="contained"
+              color="secondary"
+              onClick={async () => {
+                handlePageChange();
+              } }
+            >
+              <Typography variant="button">Switch Page</Typography>
+            </Button><Button
               variant="contained"
               color="secondary"
               onClick={async () => {
                 await logOut();
-              }}
+              } }
             >
-              <Typography variant="button">Log Out</Typography>
-            </Button>
+                <Typography variant="button">Log Out</Typography>
+              </Button></>
+
+
           ) : null}
         </Toolbar>
       </AppBar>
       {/* If a user is signed in, load the CV Item Page. Else load the sign up/log in page */}
       {/* We can add another conditional statement here to render the CV visualizer here*/}
       {/* or add a button on the CV Composition page  */}
-      {currentUser ? <CVItemsPage /> : <WelcomePage />}
+      {currentUser ?(page === "CVItemsPage" ? <CVItemsPage /> : <CVLoadPage/>) : <WelcomePage />}
     </div>
   );
 }
